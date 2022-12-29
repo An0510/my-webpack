5e4f84cbd0f3f8a245ccec56db5a471fb2048ced
AnnanPC <827479225@qq.com>
2022-12-29 08:30:15 +0800

    feat: webpack-demo分类

---
+++ webpack4-demo/src/foo.js
@@ -0,0 +1,7 @@
+import { bar } from "./bar";
+
+export function foo() {
+  let fooVal = 'fooVal'
+  console.log('foo', fooVal)
+  bar()
+}
--- webpack4-demo/webpack.config.js
+++ webpack4-demo/webpack.config.js
@@ -1,12 +1,12 @@
 const path = require('path')
 module.exports = {
-  mode:'development',
+  mode: 'development',
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
   },
-  target: 'web',
+  // target: 'web',
   module: {
     rules: [
       // 对js文件进行babel-loader处理（将ES6语法转换成ES5）
